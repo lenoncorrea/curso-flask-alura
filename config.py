0771bff3674918b4559a3dd323e3e82401a44d9a
Lenon Correa <lenonac_@hotmail.com>
2021-02-08 15:36:50 -0300

    curso flask alura

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+SECRET_KEY = 'jogoteca'
+
+MYSQL_HOST = "0.0.0.0"
+MYSQL_USER = "jogoteca"
+MYSQL_PASSWORD = "jogoteca"
+MYSQL_DB = "jogoteca"
+MYSQL_PORT = 3306
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
